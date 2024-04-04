46e54f9e6b85ca47427ad0e27797a58f0d920d17
ANUMBOSI AFESEH <aanumbosi@gmil.com>
2024-04-04 15:16:01 +0100

    note

---
+++ Lesson1/comparisonoperator.js
@@ -0,0 +1,28 @@
+var x = 5
+undefined
+true
+x > 5
+false
+x >= 5
+true
+x >= 4
+true
+x < 5
+false
+x <= 5
+true
+x == 4
+false
+x === 5
+true
+x = "5"
+"5"
+x === 5
+false
+x ==="5"
+true
+x = 5
+5
+x != 4
+true 
+x !
